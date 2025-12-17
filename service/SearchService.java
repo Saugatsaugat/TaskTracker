9e2ee2a157ec5b293012aae14ba9bc56d4711f9f
Saugat Thapa <saugatthapa2073@gmail.com>
2025-12-17 08:44:46 -0600

    Update | added methods in search service for searching by tag, due date, and priority.

--- service/SearchService.java
+++ service/SearchService.java
@@ -1,7 +1,9 @@
 package service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import enums.TaskPriority;
 import interfaces.IFilterSortSearch;
@@ -9,22 +11,38 @@ import model.Task;
 
 public class SearchService<T> implements IFilterSortSearch {
 
+    TaskService taskService = new TaskService();
+
     @Override
     public List<Task> byTag(String tag) {
-
-        return null;
+        List<Task> tasks = taskService.readAllTasks();
+        List<Task> filteredDataList = new ArrayList<>();
+        filteredDataList = tasks.stream()
+                            .filter(x->x.getTag().equals(tag))
+                            .toList();
+        return filteredDataList;
     }
 
     @Override
     public List<Task> byDueDate(LocalDate dueDate) {
+        List<Task> tasks = taskService.readAllTasks();
+        List<Task> filteredDataList = new ArrayList<>();
+        filteredDataList = tasks.stream()
+                            .filter(x -> (LocalDate.of(x.getDueDate().getYear(), x.getDueDate().getMonthValue(), x.getDueDate().getDayOfMonth())).isEqual(dueDate))
+                            .toList();
 
-        return null;
+        return filteredDataList;
     }
 
     @Override
     public List<Task> byPriority(TaskPriority priority) {
+        List<Task> tasks = taskService.readAllTasks();
+        List<Task> filteredDataList = new ArrayList<>();
+        filteredDataList = tasks.stream()
+                            .filter(x->x.getPriority().getValue().equalsIgnoreCase(priority.getValue()))
+                            .collect(Collectors.toList());
 
-        return null;
+        return filteredDataList;
     }
     
 }
