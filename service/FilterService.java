58ac0240a04c07e418b4f3745d1beb033bfd1e53
Saugat Thapa <saugatthapa2073@gmail.com>
2025-12-17 08:42:01 -0600

    Update | added methods in Filter Service to filter the list based on tag, duedate, priority.

--- service/FilterService.java
+++ service/FilterService.java
@@ -3,6 +3,7 @@ package service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import enums.TaskPriority;
 import interfaces.IFilterSortSearch;
@@ -17,21 +18,31 @@ public class FilterService<T> implements IFilterSortSearch {
         List<Task> tasks = taskService.readAllTasks();
         List<Task> filteredDataList = new ArrayList<>();
         filteredDataList = tasks.stream()
-                            .filter(x->x.getTag().equals(tag))
+                            .filter(x->x.getTag().contains(tag))
                             .toList();
         return filteredDataList;
     }
 
     @Override
     public List<Task> byDueDate(LocalDate dueDate) {
+        List<Task> tasks = taskService.readAllTasks();
+        List<Task> filteredDataList = new ArrayList<>();
+        filteredDataList = tasks.stream()
+                            .filter(x -> (LocalDate.of(x.getDueDate().getYear(), x.getDueDate().getMonthValue(), x.getDueDate().getDayOfMonth())).isEqual(dueDate))
+                            .toList();
 
-        return null;
+        return filteredDataList;
     }
 
     @Override
     public List<Task> byPriority(TaskPriority priority) {
+        List<Task> tasks = taskService.readAllTasks();
+        List<Task> filteredDataList = new ArrayList<>();
+        filteredDataList = tasks.stream()
+                            .filter(x->x.getPriority().getValue().contains(priority.getValue()))
+                            .collect(Collectors.toList());
 
-        return null;
+        return filteredDataList;
     }
     
 }
