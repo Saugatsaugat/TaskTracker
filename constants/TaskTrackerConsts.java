4bedb356605753b4ece7901b758f1941c1404d30
Saugat Thapa <saugatthapa2073@gmail.com>
2025-12-17 11:16:49 -0600

    Update | added constant for file directory

--- constants/TaskTrackerConsts.java
+++ constants/TaskTrackerConsts.java
@@ -2,4 +2,5 @@ package constants;
 
 public class TaskTrackerConsts {
     public static final String FILE_LOCATION = "//home//saugat//Desktop//JAVA//TaskTracker//tasktracker.csv";
+    public static final String FILE_DIRECTORY = "//home//saugat//Desktop//JAVA//TaskTracker";
 }
