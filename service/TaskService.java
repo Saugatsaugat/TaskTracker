ab9e03dbd3e3e2591dad14311a605bf5410305d6
Saugat Thapa <saugatthapa2073@gmail.com>
2025-12-17 14:40:16 -0600

    Bug | bug resolved, which was caused due to bad way of comparing string

--- service/TaskService.java
+++ service/TaskService.java
@@ -29,7 +29,18 @@ public class TaskService {
             line = br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
-                Task task = new Task(values[0], values[1], LocalDate.parse(values[2]), TaskPriority.valueOf(values[3]),
+                String[] dateSplit = values[2].split("-");
+
+                TaskPriority tp = TaskPriority.LOW;
+                if(values[3].equals("Low")){
+                    tp = TaskPriority.LOW;
+                }else if(values[3].equals("Medium")){
+                    tp = TaskPriority.MEDIUM;
+                }else if(values[3].equals("High")){
+                    tp = TaskPriority.HIGH;
+                }
+
+                Task task = new Task(values[0], values[1], LocalDate.of(Integer.parseInt(dateSplit[0]), Integer.parseInt(dateSplit[1]), Integer.parseInt(dateSplit[2])), tp,
                         values[4]);
                 tasks.add(task);
             }
@@ -44,7 +55,7 @@ public class TaskService {
      */
     public void writeToFile(Task task) {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileLocation, true))) {
-            String data = task.getName() + "," + task.getDueDate().toString() + "," + task.getPriority().getValue()
+            String data = task.getName() + "," + task.getTag().toLowerCase() + "," + task.getDueDate().toString() + "," + task.getPriority().getValue()
                     + "," + task.getNote();
             bw.newLine();
             bw.write(data);
