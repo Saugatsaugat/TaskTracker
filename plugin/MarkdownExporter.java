0a0e9d8ee468fed4828f63173dc4039118c23d85
Saugat Thapa <saugatthapa2073@gmail.com>
2025-12-17 11:26:40 -0600

    Update | Plugin | added logic for markdown file exporter

--- plugin/MarkdownExporter.java
+++ plugin/MarkdownExporter.java
@@ -1,11 +1,45 @@
 package plugin;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.List;
+
+import constants.TaskTrackerConsts;
+import model.Task;
+import service.TaskService;
+
 public class MarkdownExporter implements ITaskExporter {
 
     @Override
     public void export(String filename) {
-        // TODO 
+        TaskService taskService = new TaskService();
+        String fileLocation = TaskTrackerConsts.FILE_DIRECTORY+"//"+filename+".md";
 
+        //write column
+        writeColumnToFile(fileLocation);
+
+        //Read Data from file
+        List<Task> dataList = taskService.readAllTasks();
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileLocation, true))) {
+            for(Task task: dataList){
+                String data = "| " + task.getName() + " | " + task.getDueDate().toString() + " | " + task.getPriority().getValue()
+                    + " | " + task.getNote() + " |";
+            bw.newLine();
+            bw.write(data);
+            }            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
+
+    public static void writeColumnToFile(String fileLocation){
+        try {
+            String columns = "| Name | Tag | Due Date | Priority | Note |\n| :---------- | :---------: | :---------: | :--------: | -----------:";
+            FileWriter fw = new FileWriter(fileLocation);
+            fw.write(columns);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
