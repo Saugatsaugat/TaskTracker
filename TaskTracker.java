46bb65a17b7b293412217c946e675373461c5493
Saugat Thapa <saugatthapa2073@gmail.com>
2025-12-17 01:11:48 -0600

    initiated configuration to check file creation logic

--- TaskTracker.java
+++ TaskTracker.java
@@ -1,5 +1,8 @@
+import util.Configuration;
+
 public class TaskTracker{
     public static void main(String[] args) {
-        
+         // Run the initial configuration
+        Configuration conf = new Configuration();
     }
 }
